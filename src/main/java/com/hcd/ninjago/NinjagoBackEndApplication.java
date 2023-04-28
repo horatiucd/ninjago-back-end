6a9e5cb221c758b3aa93b2ef5b3ce34eaea7ac16
horatiu.dan <horatiu.dan@tangoe.com>
2023-04-28 10:12:38 +0300

    Project set-up.

---
+++ src/main/java/com/hcd/ninjago/NinjagoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hcd.ninjago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjagoBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjagoBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcd/ninjago/NinjagoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcd.ninjago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjagoBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
